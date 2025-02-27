48ec119732139d2ce60aa7653fb41a9baf07f25d
ArtemKinash47 <a5216440@gmail.com>
2025-02-27 23:04:02 +0200

    updateNew(27.02(22.03))

---
+++ src/GamePanel.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePanel extends JPanel implements KeyListener {
+    private int x = 50;
+    private int y = 50;
+
+    public GamePanel() {
+        addKeyListener(this);
+        setFocusable(true);
+    }
+    public void update() {
+        // Future game logic updates can go here
+    }
+
+    @Override public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) {
+            x -= 10;
+        } else if (key == KeyEvent.VK_RIGHT) {
+            x += 10;
+        } else if (key == KeyEvent.VK_UP) {
+            y -= 10;
+        } else if (key == KeyEvent.VK_DOWN) {
+            y += 10;
+        }
+        repaint();
+    }
+
+    @Override public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.fillRect(x, y, 30, 30);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         GameWindow window = new GameWindow();
         window.setVisible(true);
+
     }
 }
