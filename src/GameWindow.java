4bbf7065fb6b768856f3ded428d0537588d8710c
ArtemKinash47 <a5216440@gmail.com>
2025-02-27 22:01:46 +0200

    start

---
+++ src/GameWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameWindow extends JFrame {
+    public GameWindow() {
+        setTitle("OOP PROJECT");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
