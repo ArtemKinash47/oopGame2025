49141e1b6cc6581de6b3b52be84db57af12c13b9
ArtemKinash47 <a5216440@gmail.com>
2025-02-28 15:02:06 +0200

    updateNew(28.02(13.55))

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Game {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("oop projekt");
+        frame.setSize(1200, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
